c6e17d045d71dac792df4a328730e69dc61fe5d7
Nhahan <kisy324@naver.com>
2024-08-23 15:33:01 +0900

    Topic: Flow Control using if…else part1 (`a3a.rs`)

---
+++ v2/src/bin/a3a.rs
@@ -0,0 +1,10 @@
+// Topic: Flow Control using if…else
+
+fn main() {
+    let my_bool = false;
+    if my_bool == true {
+        println!("hello");
+    } else {
+        println!("goodbye");
+    }
+}
