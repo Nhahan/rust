22cb2bb7f65dcbbfc00374045ba3ad7093110fe5
Nhahan <kisy324@naver.com>
2024-08-26 08:43:55 +0900

    Topic: Topic: Working with on enum (`a7.rs`)

---
+++ v2/src/bin/a7.rs
@@ -0,0 +1,19 @@
+// Topic: Working with on enum
+
+enum Color {
+    Red,
+    Yellow,
+    Blue,
+}
+
+fn print_color(color: Color) {
+    match color {
+        Color::Red => println!("Red"),
+        Color::Yellow => println!("Yellow"),
+        Color::Blue => println!("Blue"),
+    }
+}
+
+fn main() {
+    print_color(Color::Red);
+}
