561174106d75ef4d7342ad73e64210dec301d521
Nhahan <kisy324@naver.com>
2024-08-30 23:53:43 +0900

    Topic: Ownership - wrong usage (`a11.rs`)

---
+++ v2/src/bin/a11.rs
@@ -0,0 +1,23 @@
+// Topic: Ownership
+
+struct GroceryItem {
+    quantity: i32,
+    id: i32
+}
+
+fn display_quantity(item: GroceryItem) {
+    println!("Quantity: {:?}", item.quantity);
+}
+
+fn display_id(item: GroceryItem) {
+    println!("ID: {:?}", item.id);
+}
+
+fn main() {
+    let item = GroceryItem {
+        quantity: 5,
+        id: 1
+    };
+    display_quantity(item);
+    display_id(item);
+}
