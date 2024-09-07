cd1b7d121bb9e683d303ea86e6a237013900b74c
Nhahan <kisy324@naver.com>
2024-09-08 00:47:24 +0900

    Topic: Browsing standard library documentation (`a17.rs`) - `rustup doc` 명령어를 사용하여 표준 라이브러리 문서 열기

---
+++ v2/src/bin/a17.rs
@@ -0,0 +1,9 @@
+// Topic: Browsing standard library documentation
+
+// `rustup doc` 명령어를 사용하여 표준 라이브러리 문서 열기
+
+fn main() {
+    let my_str = "this is my string";
+    pritln!("uppercase: {}", my_str.to_uppercase());
+    pritln!("lowercase: {}", my_str.to_lowercase());
+}
