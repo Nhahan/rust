089aec1e24f0a087f724b9f1953f59fe33aa1509
Nhahan <kisy324@naver.com>
2024-09-23 23:52:38 +0900

    get_input

---
+++ v2/src/bin/a20-1.rs
@@ -0,0 +1,9 @@
+use std::io;
+
+fn get_input() -> io::Result<String> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    Ok(buffer.trim().to_string())
+}
+
+fn mani() {}
