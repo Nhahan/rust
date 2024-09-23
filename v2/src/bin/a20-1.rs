e88a4809d3f590e2a9188c4ae7f3e9f18fac3d6c
Nhahan <kisy324@naver.com>
2024-09-23 23:57:08 +0900

    get_input match

--- v2/src/bin/a20-1.rs
+++ v2/src/bin/a20-1.rs
@@ -6,4 +6,19 @@ fn get_input() -> io::Result<String> {
     Ok(buffer.trim().to_string())
 }
 
-fn main() {}
+fn main() {
+    let mut all_input = vec![];
+    let mut times_input = 0;
+    while times_input < 3 {
+        match get_inut() {
+            Ok(input) => {
+                all_input.push(input);
+                times_input += 1;
+            },
+            Err(e) => {
+                println!("error: {}", e);
+                break;
+            }
+        }
+    }
+}
