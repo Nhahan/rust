3f2f063725a276039fd86eccb37afdf73d40c0ca
Nhahan <kisy324@naver.com>
2024-10-13 19:20:14 +0900

    Project1: Bill & Bills

---
+++ v2/src/bin/p1.rs
@@ -0,0 +1,30 @@
+// Project: Interaction bill manager
+
+struct Bill {
+    name: String,
+    amount: f64,
+}
+
+struct Bills {
+    inner: Vec<Bill>
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self {
+            inner: vec![]
+        }
+    }
+
+    fn add(&mut self, bill: Bill) {
+        self.inner.push(bill);
+    }
+
+    fn get_all(&self) -> &Vec<Bill> {
+        &self.inner
+    }
+}
+
+fn main() {
+
+}
